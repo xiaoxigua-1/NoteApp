f2bfecd93f73e450606e9f94e2931e72ba42fa98
xiaoxigua-1 <aijdfajodwsdf@gmail.com>
2022-06-14 20:12:14 +0800

    add button

--- app/src/main/java/com/example/xiaonote/MainActivity.kt
+++ app/src/main/java/com/example/xiaonote/MainActivity.kt
@@ -13,5 +13,7 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+        supportActionBar?.hide()
+
     }
 }

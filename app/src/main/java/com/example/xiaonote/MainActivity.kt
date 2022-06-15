9cf6e7b7472da4300a3c92edaed6916c3159e960
xiaoxigua-1 <aijdfajodwsdf@gmail.com>
2022-06-15 21:49:50 +0800

    add folder adapter

--- app/src/main/java/com/example/xiaonote/MainActivity.kt
+++ app/src/main/java/com/example/xiaonote/MainActivity.kt
@@ -1,10 +1,12 @@
 package com.example.xiaonote
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.os.Handler
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
 import com.example.xiaonote.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -22,4 +24,38 @@ class MainActivity : AppCompatActivity() {
     private fun createNote() = View.OnClickListener {
 
     }
+}
+
+class FolderData(val name: String) {
+    private val notes = mutableListOf<NotesData>()
+
+    fun createNote(context: String) {
+        notes.add(NotesData(context))
+    }
+}
+
+data class NotesData(val context: String)
+
+class Folder : RecyclerView.Adapter<Folder.FolderHolder>() {
+    val folders = mutableListOf<FolderData>()
+
+    class FolderHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        fun bind(folderName: String) {
+            itemView.findViewById<TextView>(R.id.text).text = folderName
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): FolderHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.folder_item, null)
+        view.setOnClickListener {
+
+        }
+        return FolderHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: FolderHolder, position: Int) {
+        holder.bind(folders[position].name)
+    }
+
+    override fun getItemCount(): Int = folders.size
 }
